eac148da3f0e2056f60ceb15643ad895be1e894e
Argha Rana <argharana691@gmail.com>
2026-02-14 22:42:22 +0530

    Updated backend URL

--- client/src/services/weatherService.js
+++ client/src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000/api/weather';
+const API_URL = "https://weather-app-1-xsv9.onrender.com/api/weather";
 
 export const getWeather = async (city) => {
     try {
